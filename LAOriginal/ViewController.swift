fc3b1901533e0edc431dd397c224e56e1a51b784
Kan-chann <informationjikken@gmail.com>
2024-09-16 12:05:21 +0900

    Initial Commit

---
+++ LAOriginal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LAOriginal
+//
+//  Created by Kanno Taichi on 2024/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
