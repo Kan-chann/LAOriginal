c1d24fa29d048dcc66772e5bac01e0c8c2cc1493
Kan-chann <informationjikken@gmail.com>
2024-09-22 16:47:28 +0900

    配列受け渡し

---
+++ LAOriginal/SelectKeyWordViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectKeyWordViewController.swift
+//  LAOriginal
+//
+//  Created by Kanno Taichi on 2024/09/16.
+//
+
+import UIKit
+
+class SelectKeyWordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
