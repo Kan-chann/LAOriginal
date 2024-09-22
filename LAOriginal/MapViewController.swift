5d7625a91bb46df841ef107fbebcb0e17c5f7034
Kan-chann <informationjikken@gmail.com>
2024-09-23 01:55:03 +0900

    値の受け渡しと、画面遷移完了

--- LAOriginal/MapViewController.swift
+++ LAOriginal/MapViewController.swift
@@ -1,29 +1,78 @@
-//
-//  MapViewController.swift
-//  LAOriginal
-//
-//  Created by Kanno Taichi on 2024/09/16.
-//
-
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var testMapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
+    
+    var selectDetails: [String] = ["", "", ""]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print("Selected Details after transition: \(selectDetails)") // 確認用
+        
+        // CLLocationManagerの初期化
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // 位置情報の許可をリクエスト
+        locationManager.requestWhenInUseAuthorization()
+        
+        // マップの初期表示設定（デフォルトの場所など）
+        let center = CLLocationCoordinate2D(latitude: 35.690553, longitude: 139.699579)
+        let span = MKCoordinateSpan(latitudeDelta: 0.003, longitudeDelta: 0.003)
+        let region = MKCoordinateRegion(center: center, span: span)
+        testMapView.setRegion(region, animated: true)
+        
+        // ユーザーの現在地を表示
+        testMapView.showsUserLocation = true
+        print("Selected Details: \(selectDetails)")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 位置情報の権限が変更されたときに呼ばれる
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            // 許可を求める
+            manager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            // 許可がない場合、アクションを行わない
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            // 許可された場合、位置情報の更新を開始
+            manager.startUpdatingLocation()
+        default:
+            break
+        }
     }
-    */
-
+    
+    // 位置情報が更新されたときに呼ばれる
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            // 現在地の座標を取得
+            let currentLocation = location.coordinate
+           
+            
+            // マップの中心を現在地に設定
+            let region = MKCoordinateRegion(center: currentLocation, latitudinalMeters: 500, longitudinalMeters: 500)
+            testMapView.setRegion(region, animated: true)
+            
+            // 現在地にピンを立てる
+//            let annotation = MKPointAnnotation()
+//            annotation.coordinate = currentLocation
+//            annotation.title = "現在地"
+//            testMapView.addAnnotation(annotation)
+            
+            // 位置情報の更新を停止
+            manager.stopUpdatingLocation()
+        }
+    }
+    
+    // 位置情報の取得に失敗したときに呼ばれる
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("位置情報の取得に失敗しました: \(error.localizedDescription)")
+    }
 }
--- LAOriginal/SelectKeyWordViewController.swift
+++ LAOriginal/SelectKeyWordViewController.swift
@@ -1,29 +1,73 @@
-//
-//  SelectKeyWordViewController.swift
-//  LAOriginal
-//
-//  Created by Kanno Taichi on 2024/09/16.
-//
-
 import UIKit
 
 class SelectKeyWordViewController: UIViewController {
+    @IBOutlet weak var categorySegumentControl: UISegmentedControl!
+    @IBOutlet weak var keyWordSegumentControl: UISegmentedControl!
+    @IBOutlet weak var distanceSlider: UISlider!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    var selectDetails: [String] = ["米", "高タンパク", "5"]  // 初期値を設定（配列[0], [1], [2]を確保）
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // UISliderの設定
+        distanceSlider.minimumValue = 5
+        distanceSlider.maximumValue = 15
+        distanceSlider.value = 5  // 初期値を5kmに設定
 
-        // Do any additional setup after loading the view.
+        // スライダーの値が変わった時のアクション
+        distanceSlider.addTarget(self, action: #selector(sliderValueChanged(_:)), for: .valueChanged)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func categorysegmentedControl(_ sender: UISegmentedControl) {
+        // categoryをselectDetails[0]に代入
+        selectDetails[0] = sender.titleForSegment(at: sender.selectedSegmentIndex) ?? ""
     }
-    */
+    
+    @IBAction func keyWordsegmentedControl(_ sender: UISegmentedControl) {
+        // keyWordをselectDetails[1]に代入
+        selectDetails[1] = sender.titleForSegment(at: sender.selectedSegmentIndex) ?? ""
+    }
+    
+    @objc func sliderValueChanged(_ sender: UISlider) {
+        // スライダーの現在の値を取得
+        let sliderValue = sender.value
 
+        // 値を5km, 10km, 15km
+        if sliderValue < 7.5 {
+            sender.value = 5
+        } else if sliderValue < 12.5 {
+            sender.value = 10
+        } else {
+            sender.value = 15
+        }
+
+        // 距離をselectDetails[2] にセット
+        distanceLabel.text = "\(Int(sender.value)) "
+        selectDetails[2] = distanceLabel.text ?? ""  // Optionalのためnilチェック
+    }
+
+    @IBAction func search() {
+  
+        // 検索ボタン押下時に配列を表示
+       // print("Selected Details: \(selectDetails)")
+        print("Debug Selected Details: \(selectDetails)") // 確認用
+        
+        // MapViewControllerに遷移する
+           if let mapVC = storyboard?.instantiateViewController(withIdentifier: "MapViewController") as? MapViewController {
+               // キーワードを渡す
+               mapVC.selectDetails = selectDetails
+               
+               // 画面遷移
+              // navigationController?.pushViewController(mapVC, animated: true)
+           }
+       
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "MapViewController",
+           let mapVC = segue.destination as? MapViewController {
+            mapVC.selectDetails = selectDetails
+        }
+    }
 }
